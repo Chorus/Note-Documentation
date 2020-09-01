aca681974c5f89e482daa873b7a6e99bd8a448f8
modhash <moishehalbestam@gmail.com>
2020-09-01 14:55:48 -0400

    start github io page setup

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   title: 'My Site',
   tagline: 'The tagline of my site',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://chorus.github.io/Note-Documentation/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Chorus', // Usually your GitHub org/user name.
+  projectName: 'Note-Documentation', // Usually your repo name.
   themeConfig: {
     navbar: {
       title: 'My Site',
