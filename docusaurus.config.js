c34ef30cf0033870bf69c9d78696fe53f5c8db0c
modhash <moishehalbestam@gmail.com>
2020-09-01 15:05:56 -0400

    fix url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: 'My Site',
   tagline: 'The tagline of my site',
-  url: 'https://chorus.github.io/Note-Documentation/',
+  url: 'https://chorus.github.io/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   favicon: 'img/favicon.ico',
