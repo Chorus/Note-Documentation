9c40275a33e17c3f4b7a243222b0461dc11cf969
modhash <moishehalbestam@gmail.com>
2020-09-03 14:15:38 -0400

    update url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -89,13 +89,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/Chorus/Note-Documentation/edit/master/website/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/Chorus/Note-Documentation/edit/master/website/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
