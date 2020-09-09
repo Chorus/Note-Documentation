cc6fd475367fee5b084ac076c47f42d15a1a0b1a
Shaya Gold <sgold@chorus.cloud>
2020-09-09 13:16:39 -0400

    udpated edit url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -89,7 +89,7 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/Chorus/Note-Documentation/edit/master/docs/',
+            'https://github.com/Chorus/Note-Documentation/edit/master/',
         },
         blog: {
           showReadingTime: true,
